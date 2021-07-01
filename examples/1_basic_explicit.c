9b30b06b3c43f5434b1cc50f37eb290b8e429a3f
knutsen <samuel@knutsen.co>
2021-07-01 16:45:32 +0100

    Update second example to new functionality.

--- examples/1_basic_explicit.c
+++ examples/1_basic_explicit.c
@@ -20,13 +20,13 @@ ierr main(i32 argc, const byte **argv)
 	println("Hello, %S!", name);
 
 	foreach (arg, args)
-		println("Arg passed: %S.", *arg);
+		println("Arg passed: %S.", arg);
 
 	println("In summary, that was: %D{'%S'}{, }.", args);
 
 	StringBuilder with_dots = AMAKE(byte, 0);
-	extend(&with_dots, &name, sizeof(byte));
-	extend(&with_dots, &STR("..."), sizeof(byte));
+	EXTEND(with_dots, name);
+	EXTEND(with_dots, STR("..."));
 
 	println("Goodbye, %D%c{ }", with_dots);
 
